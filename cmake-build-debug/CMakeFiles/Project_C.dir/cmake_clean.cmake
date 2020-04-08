f942c2a829ec3dc89ae046266871987925ea7cc6
13294 <13294561832@163.com>
2020-04-08 21:58:56 +0800

    Default Changelist

---
+++ cmake-build-debug/CMakeFiles/Project_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_C.dir/main.c.obj"
+  "Project_C.exe"
+  "Project_C.exe.manifest"
+  "Project_C.pdb"
+  "libProject_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
