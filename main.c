a91134457e680bd26ae597e0064be8aadad65577
13294 <13294561832@163.com>
2020-04-10 23:28:39 +0800

    排序算法之冒泡排序

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
+/*
+ * 时间：2020-4-10 22:54:58
+ * 作者：LF
+ * 邮箱;jksj27250@gmail.com
+ * 主题：排序算法
+ * 内容：
+ *      1.冒泡排序
+ *
+ * */
 #include <stdio.h>
+//冒泡排序
+int BubbleSort(int arr[],int len)
+{
+    int i_0 = 0,j_0 = 0,temp_0 = 0;
+    for(i_0 = 0;i_0 < len - 1;i_0++)
+        for(j_0 = 0;j_0 < len-1-i_0;j_0++)
+        {
+            if(arr[j_0] > arr[j_0 + 1])
+            {
+                temp_0 = arr[j_0];
+                arr[j_0] = arr[j_0+1];
+                arr[j_0+1] = temp_0;
+            }
+        }
+    return 0;
+}
 
-int main() {
-    int sun =  27+12+5+6+7+6+11+9+7+4;
-    printf("%d\n",sun);
+int main()
+{
+
+    int arr_0[] = {22,34,3,32,82,55,89,50,37,5,64,35,9,70};
+    int len_0 = (int)sizeof(arr_0)/sizeof(*arr_0);
+    int i_1 = 0;
+
+    BubbleSort(arr_0,len_0);
+
+    for(i_1 = 0; i_1 < len_0;i_1++)
+    {
+        printf("%d\n",arr_0[i_1]);
+    }
+
+    return 0;
 }
